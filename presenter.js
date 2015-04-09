c42f11f404edf1b014bdf97c4c0dbd6c583ebc28
Walter Dal Mut <walter.dalmut@gmail.com>
2015-04-09 20:05:02 +0200

    Removed mouse presenter section
    Removed mouse presenter section

--- presenter.js
+++ presenter.js
@@ -5,16 +5,4 @@
       40: "next"
     }
   });
-
-  $(document).mousedown(function(e){
-    switch(e.button) {
-      case 0:
-        Reveal.next()
-        break;
-      case 2:
-        Reveal.prev()
-        break;
-    }
-    return false;
-  });
 })();
